9571a78d66bbdaa862e29d7aa8d05ffa4632c9ef
manas <manasvini569@revature.net>
2025-12-16 00:01:57 -0600

    service discover and gateway

---
+++ Eureka/eureka-my-client/src/main/java/com/example/eureka_my_client/EurekaMyClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.eureka_my_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaMyClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaMyClientApplication.class, args);
+	}
+
+}
---
+++ Eureka/eureka-my-client/src/test/java/com/example/eureka_my_client/EurekaMyClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eureka_my_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaMyClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
